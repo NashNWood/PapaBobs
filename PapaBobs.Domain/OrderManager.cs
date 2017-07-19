554e5217e924b52e149d4df82284022bd4602284
NashNWood <nashua.wood@gmail.com>
2017-07-19 09:15:40 -0700

    Finished Working Form

---
+++ PapaBobs.Domain/OrderManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Domain
+{
+    public class OrderManager
+    {
+        public static void CreateOrder(DTO.OrderDTO orderDTO)
+        {
+            // Validation
+
+            if (orderDTO.Name.Trim().Length == 0)
+                throw new Exception("Please enter a name.");
+
+            if (orderDTO.Address.Trim().Length == 0)
+                throw new Exception("Please enter an address");
+
+            if (orderDTO.Zip.Trim().Length == 0)
+                throw new Exception("please enter a zip code.");
+
+            if (orderDTO.Phone.Trim().Length == 0)
+                throw new Exception("Please enter a phone number.");
+
+            orderDTO.OrderId = Guid.NewGuid();
+            orderDTO.TotalCost = PizzaPriceManager.CalculateCost(orderDTO);
+            Persistence.OrderRepository.CreateOrder(orderDTO);
+        }
+
+        public static void CompleteOrder(Guid orderId)
+        {
+            Persistence.OrderRepository.CompleteOrder(orderId);
+        }
+
+        public static object GetOrders()
+        {
+            return Persistence.OrderRepository.GetOrders();
+        }
+    }
+}
---
+++ PapaBobs.Web/Default.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PapaBobs.DTO.Enums;
+
+namespace PapaBobs.Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void orderButton_Click(object sender, EventArgs e)
+        {
+            if (nameTextBox.Text.Trim().Length == 0)
+            {
+                validationLabel.Text = ("Please enter name.");
+                validationLabel.Visible = true;
+                return;
+            }
+
+            if (addressTextBox.Text.Trim().Length == 0)
+            {
+                validationLabel.Text = ("Please enter an address.");
+                validationLabel.Visible = true;
+                return;
+            }
+
+            if (zipTextBox.Text.Trim().Length == 0)
+            {
+                validationLabel.Text = ("Please enter a zip code.");
+                validationLabel.Visible = true;
+                return;
+            }
+
+            if (phoneTextBox.Text.Trim().Length == 0)
+            {
+                validationLabel.Text = ("Please enter a phone number.");
+                validationLabel.Visible = true;
+                return;
+            }
+
+            try
+            {
+                var order = buildOrder();
+                Domain.OrderManager.CreateOrder(order);
+                Response.Redirect("success.aspx");
+            }
+            catch (Exception ex)
+            {
+                validationLabel.Text = (ex.Message);
+                validationLabel.Visible = true;
+                return;
+                
+            }
+            
+        }
+
+
+        private DTO.Enums.SizeType DetermineSize()
+        {
+            DTO.Enums.SizeType size;
+            if (!Enum.TryParse(sizeDropDownList.SelectedValue, out size))
+            {
+                throw new Exception("Could not determine pizza size. Please choose one of the options.");
+            }
+            return size;
+        }
+
+
+        private DTO.Enums.CrustType DetermineCrust()
+        {
+            DTO.Enums.CrustType crust;
+            if (!Enum.TryParse(crustDropDownList.SelectedValue, out crust))
+            {
+                throw new Exception("Could not determine pizza crust. Please choose one of the options.");
+            }
+            return crust;
+        }
+
+
+        private DTO.Enums.PaymentType DeterminePaymentType()
+        {
+            DTO.Enums.PaymentType paymentType;
+            if (cashRadioButton.Checked)
+            {
+                paymentType = DTO.Enums.PaymentType.Cash;
+            }
+            else 
+            {
+                paymentType = DTO.Enums.PaymentType.Credit;
+            }
+            
+            return paymentType;
+
+        }
+
+        protected void recalculateTotalCost(object sender, EventArgs e)
+        {
+            if (sizeDropDownList.SelectedValue == string.Empty) return;
+            if (crustDropDownList.SelectedValue == string.Empty) return;
+            var order = buildOrder();
+
+            try
+            {
+                string total = Domain.PizzaPriceManager.CalculateCost(order).ToString("C");
+                totalLabel.Text = total;
+            }
+            catch (Exception)
+            {
+                // Swallow the error
+            }
+           
+        }
+
+        private DTO.OrderDTO buildOrder()
+        {
+            var order = new DTO.OrderDTO();
+
+            order.Size = DetermineSize();
+            order.Crust = DetermineCrust();
+            order.Sausage = sausageCheckBox.Checked;
+            order.Pepperoni = pepperoniCheckBox.Checked;
+            order.Onions = onionsCheckBox.Checked;
+            order.GreenPeppers = greenPeppersCheckBox.Checked;
+            order.Name = nameTextBox.Text;
+            order.Address = addressTextBox.Text;
+            order.Zip = zipTextBox.Text;
+            order.Phone = phoneTextBox.Text;
+            order.PaymentType = DeterminePaymentType();
+
+            return order;
+        }
+    }
+}
